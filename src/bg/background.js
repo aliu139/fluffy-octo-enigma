95b0c8849c6fe2fa1af9e4c57dfc41e55e82cf1f
Austin Liu <arl96@cornell.edu>
2016-02-13 05:21:57 -0500

    basic timing

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,3 +1,5 @@
+var lastTime;
+
 var getUrl = function () {chrome.tabs.query({"active": true}, function(tabs){
 	var currentUrl =  tabs[0].url;
 	console.log(currentUrl);
@@ -6,4 +8,19 @@ var getUrl = function () {chrome.tabs.query({"active": true}, function(tabs){
 
 chrome.tabs.onActivated.addListener(function(tabs){
 	var currentUrl = getUrl();
+	if(lastTime){
+		var temp = new Date();
+		var temp_s = temp.getTime();
+		var diff = temp_s - lastTime;
+
+		lastTime = temp_s;
+		
+		console.log(diff);
+	}
+	else{
+		var temp = new Date();
+		var temp_s = temp.getTime();
+		lastTime = temp_s;
+		// console.log("starting timer: " + temp_s);
+	}
 });
